8c2a55f05291561984b7d0ac77e2d49688f97dfe
Jason Viox <jrv04@hotmail.com>
2021-07-28 16:27:56 -0500

    add connection const

--- Main/db/connection.js
+++ Main/db/connection.js
@@ -1,2 +1,10 @@
 const mysql = require("mysql2");
 
+const connection = mysql.createConnection({
+    host: "localhost",
+    //username
+    user: "root",
+    //password
+    password: "Goat2020!",
+    database: "employees"
+});
