cbd4ea225434570dd6ed41579f32ccf1919f2a52
Jason Viox <jrv04@hotmail.com>
2021-07-28 20:22:39 -0500

    update schema

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -13,5 +13,17 @@ CREATE TABLE role (
     salary DECIMAL NOT NULL,
     department_id INT NOT NULL,
     INDEX dep_ind (department_id),
-    
+    fk_department FOREIGN KEY (department_id) REFERENCES department (id) ON DELETE CASCADE
+);
+
+CREATE TABLE employee(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(40) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT UNSIGNED NOT NULL,
+    INDEX role_ind(role_id),
+    fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
+    manager_id INT,
+    INDEX man_ind (manager_id),
+    fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL,
 );
