758be248c9dfbd595a982146a5f5e05fe1398dc7
Jason Viox <jrv04@hotmail.com>
2021-07-29 16:54:16 -0500

    create employee function

--- Main/db/index.js
+++ Main/db/index.js
@@ -6,9 +6,18 @@ class DB {
     this.connection = connection;
 }
 
-findAllEmployees()  {
+findEmployees()  {
     return this.connection.query(
         "SELECT employee.id, employee.first_name, employee.last_name, role.title, department.name AS department, role.salary, CONCAT(manager.first_name, ' ', manager.last_name) AS manager FROM employee LEFT JOIN role on employee.role_id = role.id LEFT JOIN department on role.department_id = department.id LEFT JOIN employee manager on manager.id = employee.manager_id;"
     );
 }
+findPossibleManagers(employeeID) {
+    return this.connection.query(
+        'SELECT id, first_name, last_name FROM employe WHERE id != ?',
+        employeeId
+    );
+}
 
+createEmployee(employee) {
+    return this.connection.query("INSERT INTO employee SET ?", employee);
+}
