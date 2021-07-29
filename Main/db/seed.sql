d5cc324384b68a84950bc1dee7e48efff9e354ff
Jason Viox <jrv04@hotmail.com>
2021-07-28 20:38:21 -0500

    add roles to seed.sql

--- Main/db/seed.sql
+++ Main/db/seed.sql
@@ -10,3 +10,12 @@ VALUES
 
 INSERT INTO role
 (title, salary, department_id)
+('Sales Representative', 70000, 1),
+('Sales Manager', 90000, 1),
+('Accountant', 10000, 2),
+('Accounting Manager', 14000, 2),
+('Software Engineer', 11000, 3),
+('Lead Engineer', 14000, 3),
+('Lawyer', 16000, 4),
+('Legal Team Manager', 200000, 4);
+
